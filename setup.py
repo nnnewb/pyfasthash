8ff8bb3548b5d4744f2bd12f40fc44082256dd06
weak_ptr <weak_ptr@outlook.com>
2024-10-24 15:21:25 +0800

    ci(wheels): resolved missing development components in pypa/manylinux container
    - Ensured that CMakeLists.txt configures find_package() to locate only the
      essential development modules, addressing the incomplete component issue
      in the container environment.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ class CMakeBuild(build_ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
         cmake_args = [
             '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-            '-DPython3_ROOT_DIR='+sysconfig.get_config_vars().get('projectbase'),
+            '-DPython3_ROOT_DIR='+sysconfig.get_config_vars().get('installed_base'),
         ]
         cfg = 'Debug' if self.debug else 'Release'
         build_args = [
